b9092cd31f79aa953fa08815f2cc36bfea9922d4
Ari Elias da Silva Júnior <ari@furb.br>
2022-05-24 20:21:41 -0300

    ......

---
+++ src/Uni6Exe01.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/*
+Descreva um algoritmo que leia 10 números inteiros e os coloque em um vetor de 10 posições do tipo inteiro.
+ Escreva na ordem inversa em que foram lidos. 
+ Faça um método para ler e outro para escrever.
+*/
+public class Uni6Exe01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int[] numeros = new int[10];
+        
+        for (int i = 0; i < 10; i++) {
+            
+
+        }    
+    }
+}
