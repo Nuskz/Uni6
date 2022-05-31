06444ff9e881394320bdaf4a433ba64b6c3033db
Nuskz <arielliasjr200@gmail.com>
2022-05-31 01:33:39 -0300

    .....<

--- src/Uni6Exe01.java
+++ src/Uni6Exe01.java
@@ -6,18 +6,34 @@ Descreva um algoritmo que leia 10 números inteiros e os coloque em um vetor de 
  Faça um método para ler e outro para escrever.
 */
 public class Uni6Exe01 {
-    public static void main(String[] args) {
+    public Uni6Exe01() {
         Scanner input = new Scanner(System.in);
-        
         int[] numeros = new int[10];
-        
+        lerNumeros(input, numeros);
+        escreverNumeros(numeros);
+        input.close();
+
+    }
+
+    public int[] lerNumeros(Scanner input, int[] numeros) {
+
         for (int i = 0; i < 10; i++) {
             System.out.println("Informe o " + (i + 1) + "° número: ");
             numeros[i] = input.nextInt();
-        } 
+        }
+        return numeros;
+    }
+
+    public void escreverNumeros(int[] numeros) {
         for (int i = numeros.length - 1; i >= 0; i--) {
             System.out.println(numeros[i]);
         }
-        input.close();   
+
+    }
+
+    public static void main(String[] args) {
+
+        new Uni6Exe01();
+
     }
 }
