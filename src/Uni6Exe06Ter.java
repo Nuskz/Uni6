0b072b06c7a94a631c324139506a0192cfbec37a
Nuskz <arielliasjr200@gmail.com>
2022-05-31 00:48:04 -0300

    .....

---
+++ src/Uni6Exe06Ter.java
@@ -0,0 +1,13 @@
+/**
+ Faça um programa que leia um valor N inteiro. 
+ Com base neste valor, crie um vetor do tipo real. 
+ Em seguida, solicite ao usuário informar essa quantidade N de valores reais para popular o vetor. 
+ Após ter preenchido o vetor, solicite que o usuário informe um outro valor real. 
+ Informe para o usuário se este valor informado se encontra cadastrado no vetor. 
+ Faça um método para ler o vetor e outro, que retorne verdadeiro ou falso, para encontrar o valor.
+ */
+public class Uni6Exe06Ter {
+  public static void main(String[] args) {
+      
+  }
+}
