c457f71f7d2d9af8ec2b66d7e47977fe410d889f
Nuskz <arielliasjr200@gmail.com>
2022-06-06 18:55:43 -0300

    .a.a.a.a.a

--- src/Uni6Exe08.java
+++ src/Uni6Exe08.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
  Faça o usuário informar valores para as posições deste vetor e coloque-as nas posições na sequência informada pelo usuário. 
  Imprima uma tabela contendo cada valor diferente e o número de vezes que o valor aparece no vetor (veja exemplo a seguir).
 */
-public class Uni6Exe08Ter {
-    public Uni6Exe08Ter() {
+public class Uni6Exe08 {
+    public Uni6Exe08() {
         Scanner input = new Scanner(System.in);
         int quantidade = 0;
         quantidade = lerQuantidade(input, quantidade);
@@ -19,7 +19,7 @@ public class Uni6Exe08Ter {
     }
 
     public static void main(String[] args) {
-        new Uni6Exe08Ter();
+        new Uni6Exe08();
     }
 
     public int lerQuantidade(Scanner input, int quantidade) {
@@ -35,28 +35,28 @@ public class Uni6Exe08Ter {
         }
         return valores;
     }
-    public int[] compararValores (double[] valores, int[] frequencia){
-        
-        for (int i = 0; i < valores.length; i++){
-            for (int j = 0; j < valores.length; j++){
-                if (valores[i] == valores [j]){
+
+    public int[] compararValores(double[] valores, int[] frequencia) {
+
+        for (int i = 0; i < valores.length; i++) {
+            for (int j = 0; j < valores.length; j++) {
+                if (valores[i] == valores[j]) {
                     frequencia[i]++;
                 }
             }
         }
         return frequencia;
     }
-    public void escreverValores (double[] valores, int[] frequencia){
-        
+
+    public void escreverValores(double[] valores, int[] frequencia) {
+
         System.out.println("Valor           Frequência");
         String valoresUsados = "";
-        for(int i = 0; i < valores.length; i++){
+        for (int i = 0; i < valores.length; i++) {
             if (!valoresUsados.contains(valores[i] + "")) {
-                valoresUsados += (valores[i] + ";"); 
-                // System.out.println(valores[i] + "              " + frequencia[i]);
+                valoresUsados += (valores[i] + ";");
                 System.out.println(valores[i] + "                   " + frequencia[i]);
             }
         }
     }
 }
-
